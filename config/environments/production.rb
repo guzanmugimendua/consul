f038e563a69b65a093b3473c900b5647e0028e35
guzanmugimendua <guzanmugimendua@gmail.com>
2017-03-27 11:17:41 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: Rails.application.secrets.server_name }
   config.action_mailer.asset_host = "https://#{Rails.application.secrets.server_name}"
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {  
   address: "smtp.gmail.com",
   port: 587,
